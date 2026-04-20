23f525f6fcd5d404d14b0d26370fb780655713a4
Boominati-Dayo <mishaelekom@gmail.com>
2026-04-20 18:42:57 -0400

    Update domain to germanshepherdrehomingcenter.com

--- src/app/sitemap.xml.ts
+++ src/app/sitemap.xml.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-const baseUrl = "https://german-sherpherd-rehoming.vercel.app";
+const baseUrl = "https://germanshepherdrehomingcenter.com";
 
 export const dynamic = "force-dynamic";
 
