66213469f70d54b767da7cba815662d56341c4a9
Boominati-Dayo <mishaelekom@gmail.com>
2026-05-13 14:23:27 -0400

    Fix domain to non-www germanshepherdrehomingcenter.com

--- src/app/sitemap.xml.ts
+++ src/app/sitemap.xml.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-const baseUrl = "https://www.germanshepherdrehomingcenter.com";
+const baseUrl = "https://germanshepherdrehomingcenter.com";
 
 export const dynamic = "force-dynamic";
 
